38a14a472974407f428aa630e956f66fab997b61
zolomohan <mohanzolomon@gmail.com>
2021-04-04 17:58:29 +0530

    Update Devise Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root to: "items#index"
   
   devise_for :users
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    delete "logout", to: "devise/sessions#destroy"
+    get "signup", to: "devise/registrations#new"
+  end
   resources :categories
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
