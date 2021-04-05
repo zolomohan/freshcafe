31cfb24f57f320d7bb192520d7c33bda0b33d9a8
zolomohan <mohanzolomon@gmail.com>
2021-04-06 03:36:24 +0530

    Add Validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
     has_many :item_categories
     has_many :items, through: :item_categories
     has_one_attached :main_image
+
+    validates :name, presence: true, length: {minimum: 3, maximum: 20}
+    validates :main_image, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
     has_many :item_categories
     has_many :category, through: :item_categories
     has_one_attached :main_image
+
+    validates :main_image, presence: true
+    validates :name, presence: true, length: {minimum: 3, maximum: 30}
+    validates :description, presence: true
+    validates :price, presence: true
     
     def cart_action(current_user_id)
         if $redis.sismember "cart#{current_user_id}", id
