eb084ab8dded2749be78ff68aeb8760a6cbdd2bd
zolomohan <mohanzolomon@gmail.com>
2021-04-06 01:31:14 +0530

    Add Image for Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
     has_many :item_categories
     has_many :category, through: :item_categories
-
+    has_one_attached :main_image
+    
     def cart_action(current_user_id)
         if $redis.sismember "cart#{current_user_id}", id
             "Remove from"
