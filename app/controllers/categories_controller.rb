d6dd370a4a4a47dbf26025b9bcb9ea8620868855
zolomohan <mohanzolomon@gmail.com>
2021-04-06 01:48:22 +0530

    Add Image to Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -66,6 +66,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:name, :main_image)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
     has_many :item_categories
     has_many :items, through: :item_categories
+    has_one_attached :main_image
 end
