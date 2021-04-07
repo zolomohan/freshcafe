9c511fb49556fd507ef3d7ad01085dac7b3a09d9
zolomohan <mohanzolomon@gmail.com>
2021-04-07 07:14:49 +0530

    Add Check for Active Items in Order during Order Create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,16 +29,43 @@ class OrdersController < ApplicationController
         order = Order.new(user: current_user, user_email: current_user.email)
         order_items_data = JSON.parse(order_params)
         order.total_price = order_items_data.reduce(0) {|sum, item| sum + (item["price"] * item["quantity"].to_i) }
-        order.save
-        order_items_data.each do |item| 
-            order_item = OrderItem.new(name: item["name"], price: item["price"], quantity: item["quantity"], order_id: order.id, item_id: item["id"])
-            order_item.save
+        
+        # Check for Invalid Items
+        are_all_items_valid = true
+        order_items_data.each do |item|
+            item_db = Item.find(item["id"])
+            if !item_db.active
+                are_all_items_valid = false
+                break
+            end
+
+            item_db.category.each do |category|
+                if !category.active
+                    are_all_items_valid = false
+                    break
+                end
+            end
+
+            if !are_all_items_valid
+                break
+            end
         end
-        item_ids = $redis.smembers current_user_cart
-        redis_keys = item_ids.map {|id| "#{current_user_cart}#{id}"}
-        redis_keys.push(current_user_cart)
-        $redis.del(redis_keys)
-        redirect_to controller: 'orders', action: 'show', id: order.id
+
+        if !are_all_items_valid
+            flash[:notice] = "Some Items in your cart are not availble."
+            redirect_to root_path
+        else
+            order.save
+            order_items_data.each do |item| 
+                order_item = OrderItem.new(name: item["name"], price: item["price"], quantity: item["quantity"], order_id: order.id, item_id: item["id"])
+                order_item.save
+            end
+            item_ids = $redis.smembers current_user_cart
+            redis_keys = item_ids.map {|id| "#{current_user_cart}#{id}"}
+            redis_keys.push(current_user_cart)
+            $redis.del(redis_keys)
+            redirect_to controller: 'orders', action: 'show', id: order.id
+        end
     end
 
     def mark_delivered
